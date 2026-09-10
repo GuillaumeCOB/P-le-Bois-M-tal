0f456e4d67cbddc0565119b94f9a35fab1c1d99a
GuillaumeCOB <gsorondo@vertical-sea.com>
2026-09-10 20:05:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def inject_brand_styles(data: dict):
         color: var(--pbm-text);
     }}
     .block-container {{
-        padding-top: 1.5rem;
+        padding-top: 6rem;
         padding-bottom: 1.25rem;
         padding-left: 1.25rem;
         padding-right: 1.25rem;
